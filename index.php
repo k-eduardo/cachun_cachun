919d550984c26f951201916ccc3a749a7e5f7049
Lalo <lalo@cachun.fciencias.unam.mx>
2014-09-27 12:54:33 -0500

    primera versión

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="../pp/css/cachun.css"/>
+  <link rel="stylesheet" type="text/css" href="local.css"/>
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Averia+Sans+Libre">
+  <script type="text/javascript" src="../js/jquery/jquery.min.js"></script>
+  <script type="text/javascript" src="../js/jui/js/jquery-ui-1.10.4.js"></script>
+  <script type="text/javascript" src="../js/gsap/TwinLite.min.js"></script>
+
+  <script type="text/javascript" src="../admin/js/ajax.js"></script>
+  <script type="text/javascript" src="../admin/js/cadenas.js"></script>
+  <script type="text/javascript" src="../admin/js/interfaz.js"></script>
+  <script type="text/javascript" src="../pp/principal.js"></script>
+  <script type="text/javascript" src="../js/plugins/blurjs.min.js"></script>
+</head>
+<body class="cuerpo">
+
+<div id="pagina">
+	<header class="encabezado">
+		<div class="LogoFiltroMenu">
+			<a href='../index.php'><img id="logo" src="../img/logoT1.png"></a>
+	    	<img class="PantallaChica" id="MenuPantallaChica" src="../img/menu.gif" width="100">
+	    	<img class="PantallaChica" id="FiltroPantallaChica" src="../img/filtro.png" width="100">
+		</div>
+	        <nav><ul>
+	            <li><a href='#about-me'><div>¿Cachún?</div></a></li>
+	            <li><a href='#gallery'><div>Catálogo</div></a></li>
+	            <li><a href='#blog'><div>¡Publica!</div></a></li>
+	            <li><a href='#links'><div>Únete</div></a></li>
+	            <li><a href='#sitemap'><div>Otros Proyectos</div></a></li>
+	            <li><a href='#Video'><div>Video</div></a></li>
+	       </nav></ul>
+		<div class="ContenedorIconosContacto">
+			<a href="mailto:mochildaeideas@gmail.com"><img class="IconoArriba" src="../img/mail-icon.jpg"></a>
+			<a href="https://www.facebook.com/RevistaCachun"><img class="IconoArriba" src="../img/fb-icon.jpg"></a>
+			<a href="https://twitter.com/RevistaCachun"><img class="IconoArriba" src="../img/tw-icon.jpg"></a> 
+			<a href="https://twitter.com/RevistaCachun"><img class="IconoArriba" src="../img/tw-icon.jpg"></a> 
+		</div>
+	</header>
+</div>
+<div id="separadorH"></div>
+
+<div class="cuerpo2">
+
+	<div class="titulo">¿QUIÉNES SOMOS?</div>
+
+	<div id = "somos">
+		Vivamus tempus, urna vel semper ultricies, ipsum odio ultricies libero, sit amet gravida arcu orci fermentum neque. Vivamus ullamcorper commodo dui, mollis egestas magna dictum commodo. In eu tortor hendrerit, tincidunt nisi vitae, interdum ligula. Nullam justo nisl, volutpat in orci quis, volutpat tempor risus. Aliquam pulvinar massa eu pretium hendrerit. Suspendisse euismod ante nec metus consequat posuere. In tempor ornare dignissim. Aliquam gravida eros in viverra dictum. Pellentesque feugiat tempor dolor commodo malesuada.
+		Vivamus eget mollis odio. Praesent eu gravida mi. Phasellus id mi posuere, finibus justo eu, sollicitudin lorem. Aenean cursus, odio eget lobortis efficitur, velit purus blandit ante, a ornare odio lectus id tellus. Aliquam vulputate nulla a ex bibendum, vel varius ex iaculis. Integer congue pulvinar urna, ac porta felis aliquam sed. Quisque in augue id quam lacinia ultrices auctor et magna. Quisque pulvinar ante sodales eros dapibus, efficitur aliquam elit faucibus. Nam quis tellus urna.
+	</div>
+	<div id="comisiones">
+
+		<div class = "comision" id = "izquierda">
+				<div class = "titulo">EDITORIAL</div>
+			<div class = "contenido"><div id = "img"><img src="../img/comisiones/editorial.jpg"></div> <div id = "texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id facilisis nibh. Curabitur gravida ipsum nec mauris commodo gravida. Fusce eget dolor nunc. Nam tristique erat ut laoreet dapibus. Nullam ac turpis efficitur, laoreet mauris quis, scelerisque mauris. Sed bibendum pulvinar bibendum. Aenean ut aliquet ante, non egestas libero. Integer tempus neque quis ligula scelerisque, nec euismod arcu scelerisque. Ut finibus diam ipsum, in pretium orci egestas et. Nullam metus metus, maximus ac erat vitae, lobortis lacinia elit. Fusce purus felis, hendrerit id consequat eu, blandit at est. Ut ultricies rutrum rhoncus. Aliquam viverra magna nibh, quis laoreet odio tempor id. Nam ut arcu sollicitudin, convallis leo eget, egestas dolor.</div></div>
+		</div>
+
+		<div class = "comision" id = "derecha">
+				<div class = "titulo">GESTIÓN</div>
+			<div class = "contenido"><div id = "texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id facilisis nibh. Curabitur gravida ipsum nec mauris commodo gravida. Fusce eget dolor nunc. Nam tristique erat ut laoreet dapibus. Nullam ac turpis efficitur, laoreet mauris quis, scelerisque mauris. Sed bibendum pulvinar bibendum. Aenean ut aliquet ante, non egestas libero. Integer tempus neque quis ligula scelerisque, nec euismod arcu scelerisque. Ut finibus diam ipsum, in pretium orci egestas et. Nullam metus metus, maximus ac erat vitae, lobortis lacinia elit. Fusce purus felis, hendrerit id consequat eu, blandit at est. Ut ultricies rutrum rhoncus. Aliquam viverra magna nibh, quis laoreet odio tempor id. Nam ut arcu sollicitudin, convallis leo eget, egestas dolor.</div> <div id = "img"><img src="../img/comisiones/gestion.jpg"></div>  </div>
+		</div>
+
+		<div class = "comision" id = "izquierda">
+				<div class = "titulo">DISEÑO</div>
+			<div class = "contenido"><div id = "img"><img src="../img/comisiones/diseno.jpg"></div> <div id = "texto">Sed leo enim, consequat ut mollis nec, congue non diam. In hac habitasse platea dictumst. Aenean justo odio, facilisis eu ultricies eu, sagittis eu metus. Fusce id tellus arcu. Maecenas libero urna, sollicitudin condimentum tempor sit amet, fringilla nec massa. Donec non sodales elit, vitae lobortis orci. Suspendisse scelerisque lacinia porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer id malesuada turpis. Quisque porttitor diam nec massa venenatis, quis pellentesque odio consectetur. Nam at metus vel tortor scelerisque semper. Etiam pellentesque neque eget fermentum volutpat. In eu placerat justo, et varius erat.</div></div>
+		</div>
+
+		<div class = "comision" id = "derecha">
+				<div class = "titulo">WEB</div>
+			<div class = "contenido"><div id = "texto">Aliquam fermentum tincidunt fermentum. Suspendisse gravida odio vitae ante tempor luctus. Suspendisse potenti. In porta risus massa, ut pharetra est sollicitudin vitae. Suspendisse faucibus erat quis nunc malesuada, vitae lacinia ligula congue. Pellentesque vulputate erat nec orci tempor laoreet. Maecenas vulputate eget arcu ut rutrum. Duis scelerisque faucibus tempor. Proin vel tincidunt mauris, a maximus lorem. Sed sagittis accumsan fermentum. Nulla sed nisi eu ligula ultrices pharetra nec sed magna.</div> <div id = "img"><img src="../img/comisiones/web.jpg"></div>  </div>
+		</div>
+
+		<div class = "comision" id = "izquierda">
+				<div class = "titulo">DIFUSIÓN</div>
+			<div class = "contenido"><div id = "img"><img src="../img/comisiones/difusion.jpg"></div> <div id = "texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In congue augue ut purus consectetur pharetra tempus id velit. Fusce dapibus orci id libero vestibulum, ac tincidunt elit pellentesque. Mauris feugiat eleifend nunc luctus posuere. Quisque vitae efficitur orci. Donec eget libero vitae eros feugiat consequat. Maecenas cursus turpis et libero sagittis, eget elementum enim sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut in augue sagittis, facilisis libero eget, fringilla lectus. Quisque eu lobortis magna, in vehicula nulla.</div></div>
+		</div>
+
+	</div>
+
+</div>
+
+
+</body>
+</html>
